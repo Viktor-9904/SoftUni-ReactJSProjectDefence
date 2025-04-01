a4cc103e067677769ac0df492403d6b06ef08e73
Viktor Stanev <viktorstanev192@gmail.com>
2025-04-01 12:26:25 +0300

    Fixed a bug where the edit modal would close after clicking on it.

--- client/src/components/editPet/EditPet.jsx
+++ client/src/components/editPet/EditPet.jsx
@@ -1,15 +1,15 @@
-
-export default function EditPet({
-    PetData,
-    onClose,
-    onSubmit,
-}
-
-) {
+export default function EditPet(
+    {
+        PetData,
+        onClose,
+        onSubmit 
+    }) {
     return (
-        <div className="fixed inset-0 flex items-center justify-center backdrop-blur-md z-50" onClick={onClose}>
-            {console.log(PetData)}
-            <div className="bg-orange-300/80 rounded-lg shadow-lg p-6 w-full max-w-md relative">
+        <div className="fixed inset-0 flex items-center justify-center backdrop-blur-md z-50 bg-orange-300/50" onClick={onClose}>
+            <div
+                className="bg-orange-300/80 rounded-lg shadow-lg p-6 w-full max-w-md relative"
+                onClick={(e) => e.stopPropagation()}
+            >
                 <button
                     className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-lg"
                     onClick={onClose}
@@ -22,7 +22,7 @@ export default function EditPet({
                 </h2>
 
                 <form className="mt-4 grid gap-4">
-                    <input type="text" name="name" placeholder="Pet Name" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.name}/>
+                    <input type="text" name="name" placeholder="Pet Name" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.name} />
 
                     <select name="species" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.species}>
                         <option value="">Select Species</option>
@@ -32,7 +32,7 @@ export default function EditPet({
                         <option value="Other">Other</option>
                     </select>
 
-                    <input type="text" name="breed" placeholder="Breed" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.breed}/>
+                    <input type="text" name="breed" placeholder="Breed" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.breed} />
 
                     <input type="number" name="age" placeholder="Age (years)" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.age} />
 
@@ -44,11 +44,13 @@ export default function EditPet({
 
                     <textarea name="description" placeholder="Describe the pet..." className="w-full p-2 border rounded-md shadow-sm resize-none" rows="3" defaultValue={PetData.description}></textarea>
 
-                    <input type="text" name="imageUrl" placeholder="Photo URL" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.imageUrl}/>
+                    <input type="text" name="imageUrl" placeholder="Photo URL" className="w-full p-2 border rounded-md shadow-sm" defaultValue={PetData.imageUrl} />
 
-                    <button type="submit"
-                        className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-blue-600 transition">
-                        {/* onClick={onSubmit} */}
+                    <button
+                        type="submit"
+                        className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-blue-600 transition"
+                        onClick={onSubmit}
+                    >
                         Update
                     </button>
                 </form>
